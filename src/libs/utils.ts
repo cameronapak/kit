19eb926467363b566f7e8aedc84cdbe0540d3100
Cameron Pak <cameronandrewpak@gmail.com>
2024-10-14 10:01:47 -0500

    Added unified simple way to handle going between pages

---
+++ src/libs/utils.ts
@@ -0,0 +1,25 @@
+export const navigateByKeyboard = (event: KeyboardEvent) => {
+  const target = event.target as HTMLElement & { localName: string };
+  // Check if the focus is not on an input element
+  if (
+    !(
+      event.target instanceof HTMLInputElement ||
+      event.target instanceof HTMLTextAreaElement ||
+      target?.localName === "trix-editor"
+    )
+  ) {
+    if (event.key === "1") {
+      window.location.href = "/dashboard";
+    } else if (event.key === "2") {
+      window.location.href = "/dashboard/posts";
+    } else if (event.key === "3") {
+      window.location.href = "/dashboard/emails";
+    }
+  }
+};
+
+export const setupKeyboardNavigation = () => {
+  document.addEventListener("keydown", (event: KeyboardEvent) => {
+    navigateByKeyboard(event);
+  });
+};
