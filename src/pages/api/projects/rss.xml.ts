1ffb6dbcd31aac2b54402c92a701c8d1f4f3dccc
Cameron Pak <cameronandrewpak@gmail.com>
2024-11-02 22:20:46 -0500

    Updated RSS file

--- src/pages/api/projects/rss.xml.ts
+++ src/pages/api/projects/rss.xml.ts
@@ -4,19 +4,31 @@ import { db, eq, Projects, desc } from "astro:db";
 import { getCldImageUrl } from "astro-cloudinary/helpers";
 
 export const GET: APIRoute = async ({ request }) => {
-  const projects = await db.select()
+  const projects = await db
+    .select()
     .from(Projects)
     .where(eq(Projects.isPublished, true))
     .orderBy(desc(Projects.createdAt));
 
   const items: RSSFeedItem[] = await Promise.all(
     projects.map(async (project) => {
-      const logoImageUrl = project.logoImageId ? await getCldImageUrl({
-        src: project.logoImageId,
-        width: 256,
-        crop: "fill",
-        format: "webp"
-      }) : null;
+      const logoImageUrl = project.logoImageId
+        ? await getCldImageUrl({
+            src: project.logoImageId,
+            width: 256,
+            crop: "fill",
+            format: "webp"
+          })
+        : null;
+
+      const bannerImageUrl = project.bannerImageId
+        ? await getCldImageUrl({
+            src: project.bannerImageId,
+            width: 512,
+            crop: "fill",
+            format: "webp"
+          })
+        : null;
 
       const item: RSSFeedItem = {
         title: project.title,
@@ -24,7 +36,11 @@ export const GET: APIRoute = async ({ request }) => {
         pubDate: project.createdAt,
         link: `/app/${project.slug}`,
         // Add a link to the project's own RSS feed
-        customData: `<projectRssFeed>/api/posts/rss.xml?projectId=${project.id}</projectRssFeed>`
+        customData: `
+          <projectRssFeed>/api/posts/rss.xml?projectId=${project.id}</projectRssFeed>
+          ${bannerImageUrl ? `<bannerImageUrl>${bannerImageUrl}</bannerImageUrl>` : ""}
+          ${logoImageUrl ? `<logoImageUrl>${logoImageUrl}</logoImageUrl>` : ""}
+        `.trim()
       };
 
       if (logoImageUrl) {
